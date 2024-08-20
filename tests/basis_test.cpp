fb8343a40c48e824d84c6627bc54bf89b07dddc6
WariorHanamy <632029496@qq.com>
2024-08-21 07:53:50 +0800

    Add coefs to test if meets theorectical thinking, it is OK!

--- tests/basis_test.cpp
+++ tests/basis_test.cpp
@@ -10,7 +10,9 @@
 #include <fstream>
 #include <iostream>
 #include <limits>
+#include <ranges>
 #include <rclcpp/rclcpp.hpp>
+#include <tuple>
 #include <vector>
 
 int main(int argc, char **argv) {
@@ -18,26 +20,22 @@ int main(int argc, char **argv) {
   using Spline = bspline::Spline<double, SPLINE_DEGREE>;
 
   // Define knots vector.
-  const std::vector<double> knots{0.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0};
+  const std::vector<double> knots{0.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 5.0, 5.0};
 
   // Generate Splines.
   const std::vector<Spline> spl =
       bspline::generateBSplines<SPLINE_DEGREE>(knots);
+
+  std::vector<double> coeffs(spl.size(), 1.0);
   std::ofstream out("diffusion.csv");
   out.precision(std::numeric_limits<double>::max_digits10);
-
+  std::cout << spl.size();
   for (double x = -1.0; x <= 6.0; x += 0.01) {
     out << x << ",";
-    for (auto &s : spl) {
-      if (s == spl.back()) {
-        out << s(x) << "\n";
-      } else {
-        out << s(x) << ",";
-      }
+    double val = 0;
+    for (size_t i = 0; i < spl.size(); i++) {
+      val += spl[i](x) * coeffs[i];
     }
-    // for (data_t x = static_cast<data_t>(-10); x <= static_cast<data_t>(10);
-    //      x += static_cast<data_t>(1) / 100) {
-    //   out << x << "\t" << diffCoeff(x) << "\t" << solution(x) << "\n";
-    // }
+    out << val << "\n";
   }
 }
